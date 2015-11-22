de7bf486ed222306d43124788ad6db9657e6f7d2
omri <omri@singular.net>
2015-11-22 23:52:29 +0000

    better line breaks

--- playground-app/app/src/main/java/com/bla/patch/TextReverser.java
+++ playground-app/app/src/main/java/com/bla/patch/TextReverser.java
@@ -2,8 +2,11 @@ package com.bla.patch;
 
 
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Pair;
 
+import java.awt.font.TextAttribute;
 import java.text.Bidi;
 
 public class TextReverser {
@@ -14,13 +17,22 @@ public class TextReverser {
         int flags = 0;
         Bidi bidi = new Bidi(input, flags);
         if (bidi.isLeftToRight()) {
-            Log.d(TAG, String.format("Not for me: %s", input));
+//            Log.d(TAG, String.format("Not for me: %s", input));
             return input;
         }
         String res2 = "";
         while (input.length() > start) {
 //            Log.d(TAG, String.format("Going to do: createLineBidi(%d, %d)", start, Math.min(lineLimit, input.length() - start)));
-            Bidi lineBidi = bidi.createLineBidi(start, Math.min(start + lineLimit, input.length()));
+            Pair<Integer, Integer> lineEndParams = findCurrentLineEnd(input, start, lineLimit);
+            int curLineEnd = lineEndParams.first;
+            int skipCount = lineEndParams.second;
+
+            // just a safety
+            if ((curLineEnd == start) && (skipCount == 0)) {
+                throw new Exception("Seems like we're not making any progress here...");
+            }
+
+            Bidi lineBidi = bidi.createLineBidi(start, curLineEnd);
             String line_input = input.substring(start, start + lineBidi.getLength());
             if (start > 0) {
                 res2 += "\n";
@@ -28,13 +40,71 @@ public class TextReverser {
             res2 += reverseLine(line_input);
             start += lineBidi.getLength();
 
-            // just a safety
-            if (lineBidi.getLength() == 0) {
-                throw new Exception("Seems like we're not making any progress here...");
-            }
+            start += skipCount;
         }
 //        tv.setText(res + "\n\n" + res2);
         return res2;
+    }
+
+    private static Pair<Integer, Integer> findCurrentLineEnd(String input, int start, int lineLimit) {
+        int lineEnd = Math.min(start + lineLimit, input.length());
+        boolean skipChar = false;
+        boolean skippedLineBreak = false;
+        int skipCount = 0;
+        // line breaks take priority
+        for (int i = start; i < lineEnd; i++) {
+            if (input.charAt(i) == '\n') {
+                skipChar = true;
+                skippedLineBreak = true;
+                lineEnd = i;
+                break;
+            }
+        }
+
+        // if no line breaks and the result is the whole string, leave it as it is
+        if (lineEnd == input.length()) {
+            return new Pair<>(lineEnd, 0);
+        }
+
+        // no line break found, search for a white space
+        boolean shouldStop = false;
+        for (int currentChar = lineEnd - 1; (currentChar > start) && (!shouldStop); currentChar--) {
+            switch (input.charAt(currentChar)) {
+                // TODO: find a generic way to determine white spaces
+                case ' ':
+                case '\t':
+                    skipChar = true;
+                    lineEnd = currentChar;
+                    shouldStop = true;
+                    break;
+            }
+        }
+        if (skipChar) {
+            skipCount = 1;
+            while (((lineEnd + skipCount) < input.length()) &&
+                    isAllowedWhitespace(input, lineEnd + skipCount, skippedLineBreak)) {
+                if (input.charAt(lineEnd + skipCount) == '\n') {
+                    skippedLineBreak = true;
+                }
+                skipCount++;
+            }
+            while ((lineEnd > start) &&
+                    isAllowedWhitespace(input, lineEnd - 1, skippedLineBreak)) {
+                if (input.charAt(lineEnd - 1) == '\n') {
+                    skippedLineBreak = true;
+                }
+                lineEnd--;
+                skipCount++;
+            }
+        }
+        return new Pair<>(lineEnd, skipCount);
+    }
+
+    private static boolean isAllowedWhitespace(String input, int position,
+                                               boolean skippedLineBreak) {
+        return ((input.charAt(position) == ' ') ||
+                (input.charAt(position) == '\t') ||
+                ((!skippedLineBreak) && (input.charAt(position) == '\n')));
     }
 
     @NonNull
