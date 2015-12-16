89a107ddda82828cc15a091b232b45268083d2b0
omri <omri@singular.net>
2015-12-17 00:51:55 +0200

    new version (3.8.0)

--- unpack-pebble-app.sh
+++ unpack-pebble-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=3.7.0-908
+VERSION=3.8.0-941-3eac5bc
 
 ./apktool-files/apktool d -v -p apktool-files/framework -o pebble-unpacked-apk input-apks/com.getpebble.android.basalt-$VERSION.apk
